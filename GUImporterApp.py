1646789bf4a3935f1ab18cd42e4b93d3ccebfe3f
aulloa <adu8917@rit.edu>
2016-02-09 21:30:06 -0500

    Initial Commit

---
+++ GUImporterApp.py
@@ -0,0 +1,85 @@
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+import numpy
+from numpy import loadtxt
+from io import StringIO
+from ast import literal_eval
+from functools import partial
+
+class GUImporterApp(App):
+    def build(self):
+        self.layout = GridLayout(cols=2,row_force_default=True, row_default_height=40)
+        l  = Label(text='Start Row')
+        l2 = Label(text='Columns(Comma Seperated)')
+        self.layout.add_widget(l)
+        self.layout.add_widget(l2)
+        self.txt1 = TextInput(text='0', multiline=False)
+        self.txt2 = TextInput(text='0,1', multiline=False)
+        self.layout.add_widget(self.txt1)
+        self.layout.add_widget(self.txt2)
+        btn1 = Button(text="RUN")
+        btn1.bind(on_press=self.buttonClicked)
+        self.layout.add_widget(btn1)
+
+        #l3 = Label(text='Data File')
+        #self.layout.add_widget(l3)
+        #self.txt1.bind(text = self.importdatfile)
+        #self.txt1.bind(text = l3.setter('text'))
+        self.txt1.bind(text=partial(self.importdatfile,self))
+##      
+        
+        return self.layout
+
+    def importdatfile(self,*args):
+        Rows = self.txt1.text
+        Cols = self.txt2.text
+        l = literal_eval(Cols)
+        #print(l)
+        x = numpy.loadtxt('ET.csv',
+                      comments='#',
+                      delimiter=',',
+                      converters=None,
+                      skiprows=int(float(Rows)),
+                      usecols=l,
+                      unpack=False,
+                      ndmin=0)
+        x2 = " ".join("\t".join(map(str,l)) for l in x)
+        print(type(x2))
+        l3 = Label(text=x2)
+        self.layout.add_widget(l3)
+        print(x2)
+        
+
+    def buttonClicked(self,btn):
+        Rows = self.txt1.text
+        Cols = self.txt2.text
+        b    = len(Cols)
+        l = literal_eval(Cols)
+        print(l)
+        x = numpy.loadtxt('ET.csv',
+                      comments='#',
+                      delimiter=',',
+                      converters=None,
+                      skiprows=int(float(Rows)),
+                      usecols=l,
+                      unpack=False,
+                      ndmin=0)
+        print(x)
+        numpy.savetxt('VT.txt',
+                      x,
+                      fmt='%.18e',
+                      delimiter=' ',
+                      newline='\r\n',
+                      header='',
+                      footer='',
+                      comments='# ')
+        
+
+if __name__ == "__main__":
+    GUImporterApp().run()
